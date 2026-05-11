cab6ed37c516b95493c0e24c401c9b5c572b4c93
Andirell <andikanudom123@gmail.com>
2026-05-11 05:29:48 -0700

    updated

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,2 @@
 from django.test import TestCase
 
-"""Tests intentionally removed.
-
-This module formerly included automated tests. Manual testing is
-recommended using Postman/Swagger as per repository preference.
-"""
